a0a45f228f056da14c0bb7dff6818d92ba45f1db
Manoel Lima <manoelsslima@yahoo.com.br>
2025-11-11 00:22:29 -0400

    feat: use LinkedHashSet for maintaining order of menu categories

--- src/main/java/br/com/manoel/cardapio/Main.java
+++ src/main/java/br/com/manoel/cardapio/Main.java
@@ -10,7 +10,7 @@ public class Main {
         List<ItemCardapio> itens = database.listaDeItensCardapio();
 
         // saber quais categorias existem no cardápio
-        Set<ItemCardapio.CategoriaCardapio> categorias = new HashSet<>();
+        Set<ItemCardapio.CategoriaCardapio> categorias = new LinkedHashSet<>();
         for (ItemCardapio item : itens) {
             ItemCardapio.CategoriaCardapio categoria = item.categoria();
             categorias.add(categoria);
