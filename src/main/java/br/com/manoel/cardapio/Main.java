fa37356bc7e2135d83a2528f05b82982e5d72545
Manoel Lima <manoelsslima@yahoo.com.br>
2025-11-11 00:22:12 -0400

    feat: display unique categories from menu items

--- src/main/java/br/com/manoel/cardapio/Main.java
+++ src/main/java/br/com/manoel/cardapio/Main.java
@@ -1,14 +1,26 @@
 package br.com.manoel.cardapio;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         Database database = new Database();
         List<ItemCardapio> itens = database.listaDeItensCardapio();
-        for (ItemCardapio itemCardapio : itens) {
-            System.out.println(itemCardapio);
+
+        // saber quais categorias existem no cardápio
+        Set<ItemCardapio.CategoriaCardapio> categorias = new HashSet<>();
+        for (ItemCardapio item : itens) {
+            ItemCardapio.CategoriaCardapio categoria = item.categoria();
+            categorias.add(categoria);
         }
+        for (ItemCardapio.CategoriaCardapio categoria : categorias) {
+            System.out.println(categoria);
+        }
+        System.out.println("---------------------");
+        itens.stream().map(ItemCardapio::categoria)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
     }
 }
